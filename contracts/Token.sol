05d814018414ab10526125abf4dfa698e9759645
aaronjan98 <aaronjan98@gmail.com>
2022-11-15 12:44:57 -0800

    token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,6 +10,10 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -18,5 +22,21 @@ contract Token {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
