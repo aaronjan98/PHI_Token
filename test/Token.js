3d3694b3b2fa0d7e5e9e7c120ef459591f98e825
aaronjan98 <aaronjan98@gmail.com>
2022-11-15 11:06:14 -0800

    create tests for decimals and totalSupply

--- test/Token.js
+++ test/Token.js
@@ -1,12 +1,47 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers')
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-describe("Token", () => {
-  it("has a name", async () => {
-    const Token = await ethers.getContractFactory("Token");
-    let token = await Token.deploy();
+// async function deployTokenFixture() {
+//   const Token = await ethers.getContractFactory('Token')
+//   token = await Token.deploy()
+//   await token.deployed()
 
-    const name = await token.name();
-    expect(name).to.equal("My Token");
-  });
-});
+//   return { token }
+// }
+
+const tokens = n => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Token', () => {
+  let token
+
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory('Token')
+    token = await Token.deploy('Phi Token', 'PHI', '1000000')
+  })
+
+  describe('Deployment', async () => {
+    const name = 'Phi Token'
+    const symbol = 'PHI'
+    const decimals = '18'
+    const totalSupply = tokens('1000000')
+
+    it('has correct name', async () => {
+      expect(await token.name()).to.equal(name)
+    })
+
+    it('has correct symbol', async () => {
+      expect(await token.symbol()).to.equal(symbol)
+    })
+
+    it('has correct decimals', async () => {
+      expect(await token.decimals()).to.equal(decimals)
+    })
+
+    it('has correct total supply', async () => {
+      expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+  })
+})
